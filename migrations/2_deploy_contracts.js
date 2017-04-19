11282b440d2bdfce05bef8794717d98e7cbace6f
Marco Polci <mpolci@gmail.com>
2017-04-19 12:01:26 +0200

    Delete unused files

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const DutchAuction = artifacts.require("./DutchAuction.sol");
-const ProxySender = artifacts.require("./ProxySender.sol");
-const Token = artifacts.require("./Token.sol");
-
-module.exports = function(deployer, network, accounts) {
-  // deployer.deploy(DuchAuction, accounts[0], 284000000000000000000000, 5114)
-  //   .then(() => DuchAuction.setup(/* token */))
-  //   .then(() => deployer.deploy(ProxySender, DuchAuction.address))
-};
